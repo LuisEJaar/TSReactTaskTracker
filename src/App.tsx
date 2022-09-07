7268797c88c7d211968243b53e40e12be3ffc842
LuisEJaar <luisjaarcoding@gmail.com>
2022-09-06 20:32:33 -0500

    Completed up to edit button

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+import './App.css';
+import InputField from './components/InputField'
+import {Todo} from "./model"
+import TodoList from "./components/TodoList"
+
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("")
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault()
+    if(todo) {
+      setTodos([...todos, {id: Date.now(), todo: todo, isDone: false}])
+      setTodo("")
+    }
+  }
+  console.log(todos)
+
+  return (
+    <div className="App">
+      <span className='heading'>Taskify</span>
+      <InputField handleAdd={handleAdd} todo={todo} setTodo={setTodo}/>
+      <TodoList todos={todos} setTodos={setTodos}/>
+    </div>
+  );
+}
+
+export default App;
